5fe9a88190ed7c939706aac8ef8231637169d7b4
rogeriofabriciodev <60872513+rogeriofabriciodev@users.noreply.github.com>
2021-01-28 05:30:42 -0300

    init
    init

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("It's Great!!!");
+  res.send("It's Great!!!!");
   console.log("Sevidor Rodando Docker");
 });
 
